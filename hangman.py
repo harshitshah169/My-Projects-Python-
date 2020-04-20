ff6064b06c0898a34a31776996de3563b0a7a76b
harshit shah <dinkushah169@gmail.com>
2020-04-20 17:42:12 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -8,6 +8,7 @@ ATTEMPTS = 5
 def if_lost(ATTEMPTS,GAME_OVER):
 
     if ATTEMPTS == 0:
+        print('The word was ' + str(word).upper())
         print("\nYOU LOST, BETTER LUCK NEXT TIME!!!")
         GAME_OVER = True
 
@@ -43,8 +44,8 @@ ans = input("\nType 'y' to start the game : ").casefold()
 
 while ans == 'y':
 
-    random_word = np.array(['hella', 'india', 'pakistan', 'australia', 'japan'])
-    word = str(random_word[random.randint(0, 4)])
+    random_word = np.array(['airplane','angle','Ann','apple','art','Atlantic','atmosphere','bar','barn','baseball','beauty','believed','bell','belong','beneath','bigger','bottle','bowl','broad','chapter','chart','Chinese','clearly','climate','clock','closely','clothing','coffee','cow','cry','Dad','dangerous','deer','desk','detail','development','drew','driver','event','everywhere','fat','favorite','fence','fifty','flight','flow','flower','forget','fourth','friendly','generally','German','Germany','giant','golden','grain','handle','height','hung','hurry','immediately','industry','instance','Italy','James','knife','lake','Latin','','leader','leaving','likely','lunch','mass','master','mile','mix','model','mud','muscle','nearby','nearest','nest','newspaper','nobody','observe','Pacific','peace','plate','plenty','popular','powerful','push','railroad','rapidly','root','rubber','sad','sail','save','score','seeing','serious','service','sheet','shop','silent','smell','smoke','smooth','source','spell','storm','structure','supper','support','sweet','swim','telephone','Texas','threw','throw','tone','tool','track','trail','understanding','upper','view','wagon','western','whatever','wheat','whenever','whom','win','wonderful','wore'])
+    word = str(random_word[random.randint(0,len(random_word))])
     word_len = len(word)
     hidden_word = list("_" * word_len)
     used = str(' ')
